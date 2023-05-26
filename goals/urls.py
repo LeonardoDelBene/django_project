c6dfdc9663beaed795746d33a613a1e67d533ee2
Delbo01 <leonardo.delbene@stud.unifi.it>
2023-05-26 14:56:16 +0200

    change template

--- goals/urls.py
+++ goals/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import get_all_goals
+from .views import get_all_goals, create_goals, view_completed_goals
+
 urlpatterns = [
     path('', get_all_goals, name='goals'),
+    path('create/', create_goals, name='create_goals'),
+    path('viewCompletedGoal/',view_completed_goals, name='view_completed_goals'),
 ]
--- goals/views.py
+++ goals/views.py
@@ -14,3 +14,28 @@ def get_all_goals(request):
             return render(request, 'goals.html', context)
     else :
         return redirect('login')
+
+def create_goals(request):
+    if(request.user.is_authenticated):
+        if(request.method == 'POST'):
+            user=request.user
+            goal=request.POST['goal']
+            start_date=request.POST['start_date']
+            end_date=request.POST['end_date']
+            completed=request.POST['completed']
+            Goal.objects.create(user=user, goal=goal, start_date=start_date, end_date=end_date, completed=completed)
+            return redirect('profile_home')
+        else:
+            return render(request, 'create_goals.html')
+    else :
+        return redirect('login')
+
+def view_completed_goals(request):
+    if(request.user.is_authenticated):
+        if(request.method == 'GET'):
+            user=request.user
+            goals = Goal.objects.filter(user=user, completed=True)
+            context={
+                'goals': goals
+            }
+            return render(request, 'goals.html', context)
