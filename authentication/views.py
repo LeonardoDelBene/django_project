548be75b26e074d2453e2b6b0d256e5f7596534a
Delbo01 <leonardo.delbene@stud.unifi.it>
2023-06-06 13:28:16 +0200

    add workouts app

--- authentication/views.py
+++ authentication/views.py
@@ -9,4 +9,4 @@ class SignUpView(generic.CreateView):
     template_name = 'register.html'
 
 def homePage_view(request):
-    return render(request, 'homePage.html')
+    return render(request, 'index.html')
