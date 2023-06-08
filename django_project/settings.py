78d7d42d635ca9b36fa05a285cc985da39f570bc
Delbo01 <leonardo.delbene@stud.unifi.it>
2023-06-08 17:37:56 +0200

    deploy

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_SECURE = True  # Imposta su True se stai utilizzando HTTPS, False altrimenti
+
