5077992366ffe3c6f80c68e91c825ae76c73efed
Delbo01 <leonardo.delbene@stud.unifi.it>
2023-06-08 18:58:11 +0200

    deploy

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
